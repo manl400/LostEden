4f5bc6827bde0fd1fb33924d4c67b072988ff11d
Derek Slager <lostrevi@gmail.com>
2014-05-17 00:12:58 -0500

    if added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,16 @@
 class WelcomeController < ApplicationController
   def index
     @homePageText = "Welcome to a lost Eden!"
+   
+    
+    if Time.now.strftime('%A') == 'Saturday'
+      
+      @i0 = "variable is Saturday!"
+    else
+      @i0 = "not a good day i guess."
+      
+     
+    end
+    
   end
 end
